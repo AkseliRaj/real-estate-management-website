a248cacb5aa2c2e20d20ebe1400227741265c409
AkseliRaj <akseli.rajahalme@gmail.com>
2026-04-07 13:42:52 +0300

    Add Prefix Section to Property Management Forms Page and update FormIntroductionSection component for dynamic content rendering

--- ar-isannointi-oy/src/components/FormIntroductionSection.jsx
+++ ar-isannointi-oy/src/components/FormIntroductionSection.jsx
@@ -6,28 +6,69 @@ const rowJustifyClass = {
 export default function FormIntroductionSection({
     title,
     paragraphs = [],
+    bulletListIntro,
+    bulletListItems = [],
+    contentItems = [],
     innerContainer = true,
     rowJustify = 'start',
 }) {
     const rowClassName = `row ${rowJustifyClass[rowJustify]} py-5`;
+    const hasOrderedContent = contentItems.length > 0;
 
     const inner = (
         <div className={rowClassName}>
             <div className='col-12 col-lg-8 col-xxl-8 py-0 py-md-5'>
                 <h3 className='d-none d-md-block pb-1 pb-sm-2 pb-lg-3'>{title}</h3>
                 <h4 className='d-block d-md-none pb-1 pb-sm-2 pb-lg-3'>{title}</h4>
-                {paragraphs.map((text, index) => (
-                    <p
-                        key={index}
-                        className={
-                            index === paragraphs.length - 1
-                                ? 'pb-1 pb-sm-2 pb-lg-3'
-                                : undefined
+                {hasOrderedContent
+                    ? contentItems.map((item, index) => {
+                        if (item.type === 'paragraph') {
+                            return (
+                                <p key={`${item.type}-${index}`} className='pb-1 pb-sm-2 pb-lg-3'>
+                                    {item.text}
+                                </p>
+                            );
                         }
-                    >
-                        {text}
-                    </p>
-                ))}
+
+                        if (item.type === 'bulletList') {
+                            return (
+                                <div key={`${item.type}-${index}`} className='pb-1 pb-sm-2 pb-lg-3'>
+                                    {item.intro ? <p>{item.intro}</p> : null}
+                                    <ul className='mb-0'>
+                                        {item.items.map((bulletItem, bulletIndex) => (
+                                            <li key={bulletIndex}>{bulletItem}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            );
+                        }
+
+                        return null;
+                    })
+                    : (
+                        <>
+                            {paragraphs.map((text, index) => (
+                                <p
+                                    key={index}
+                                    className={
+                                        index === paragraphs.length - 1
+                                            ? 'pb-1 pb-sm-2 pb-lg-3'
+                                            : undefined
+                                    }
+                                >
+                                    {text}
+                                </p>
+                            ))}
+                            {bulletListIntro ? <p>{bulletListIntro}</p> : null}
+                            {bulletListItems.length > 0 ? (
+                                <ul className='pb-1 pb-sm-2 pb-lg-3'>
+                                    {bulletListItems.map((item, index) => (
+                                        <li key={index}>{item}</li>
+                                    ))}
+                                </ul>
+                            ) : null}
+                        </>
+                    )}
             </div>
         </div>
     );
--- ar-isannointi-oy/src/pages/PropertyManagementFormsPage.jsx
+++ ar-isannointi-oy/src/pages/PropertyManagementFormsPage.jsx
@@ -2,9 +2,35 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import IntroductionSectionWithImage from '../components/IntroductionSectionWithImage';
 import FormsPageIntroductionImage from '../assets/webp/FormPageIntroductionImage.webp';
+import FormIntroductionSection from '../components/FormIntroductionSection';
 
 const PropertyManagementFormsPage = () => {
     const { t } = useTranslation();
+    const prefixSection = t('Forms-Page.Prefix-Section', { returnObjects: true });
+
+    const contentItems = Object.entries(prefixSection)
+        .filter(([key]) => key !== 'title')
+        .map(([key, value]) => {
+            if (key.startsWith('paragraph') && typeof value === 'string') {
+                return {
+                    type: 'paragraph',
+                    text: value,
+                };
+            }
+
+            if (key === 'bullet-point-list' && typeof value === 'object' && value !== null) {
+                return {
+                    type: 'bulletList',
+                    intro: value.paragraph1,
+                    items: Object.entries(value)
+                        .filter(([bulletKey, bulletValue]) => bulletKey.startsWith('bullet-point-paragraph') && typeof bulletValue === 'string')
+                        .map(([, bulletValue]) => bulletValue),
+                };
+            }
+
+            return null;
+        })
+        .filter(Boolean);
 
     return (
         <div className="container-fluid px-0">
@@ -16,6 +42,13 @@ const PropertyManagementFormsPage = () => {
                 ]}
                 imageSrc={FormsPageIntroductionImage}
                 imageAlt={t('propertyLanding.images.introductionAlt')}
+            />
+
+            <FormIntroductionSection
+                title={prefixSection.title}
+                contentItems={contentItems}
+                innerContainer={false}
+                rowJustify="center"
             />
 
         </div>
